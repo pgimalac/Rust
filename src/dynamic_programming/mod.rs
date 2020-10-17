fc294ceb33ea7f80df021d037aae3ffb8245d736
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-10-17 11:29:58 +0200

    correction of a rustfmt issue

--- src/dynamic_programming/mod.rs
+++ src/dynamic_programming/mod.rs
@@ -1,15 +1,15 @@
 mod edit_distance;
 mod egg_dropping;
 mod fibonacci;
-mod longest_increasing_subsequence;
 mod longest_common_subsequence;
+mod longest_increasing_subsequence;
 
 pub use self::edit_distance::{edit_distance, edit_distance_se};
 pub use self::egg_dropping::egg_drop;
 pub use self::fibonacci::fibonacci;
 pub use self::fibonacci::recursive_fibonacci;
+pub use self::longest_common_subsequence::longest_common_subsequence;
 pub use self::longest_increasing_subsequence::{
     longest_increasing_subsequence, longest_increasing_subsequence_by,
     longest_strictly_increasing_subsequence,
 };
-pub use self::longest_common_subsequence::longest_common_subsequence;
